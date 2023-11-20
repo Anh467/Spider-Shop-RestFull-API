8b6bce95095084559cb08a8319fd328bd32c88f6
Nguyen Anh Viet <van123872000@gmail.com>
2023-11-20 11:45:03 +0700

    first commit

---
+++ schema/Create.sql
@@ -0,0 +1,20 @@
+create database SpiderShop;
+use  SpiderShop;
+-- ########################### dbo.User ###########################
+create table User(
+    UserID int primary key auto_increment not null unique,
+    Account varchar(100) unique not null,
+    Password varchar(255) not null, -- Consider using a larger size for password hash
+    Name nvarchar(50) not null,
+    Image varchar(200) default "",
+    Gender varchar(10) default "Others",
+    Phone varchar(20) default "",
+    Mail varchar(100) default "" check (Mail = '' or Mail like '%@%.%'), -- Basic email format validation
+    Birth datetime default current_timestamp,
+    Role varchar(10) default "User",
+    Expenditure float8 default 0,
+    NumOrder int default 0,
+    createdAt datetime default current_timestamp,
+	updatedAt datetime default current_timestamp
+);
+-- tracking
