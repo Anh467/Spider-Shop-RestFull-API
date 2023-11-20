845a5209cf1be8cf4e3d196fb6ef86e830753421
Nguyen Anh Viet <van123872000@gmail.com>
2023-11-20 16:10:19 +0700

    add middleware for recovery

---
+++ golang/middleware/recovery.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	common "SpiderShop-Restfull-API/common"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// defer recovery
+		defer func() {
+			// check if recovery is already in progress or not
+			if err := recover(); err != nil {
+				// cast err to Error HandlerFunc
+				if errorHandler, ok := err.(*common.ErrorHandler); ok {
+					// response to user error handler
+					c.JSON(errorHandler.ErrorCode, gin.H{
+						"Error": gin.H{
+							"code":    errorHandler.ErrorCode,
+							"message": errorHandler.ErrorMessage,
+						},
+					})
+					// simultaneously stop middleware and return
+					c.Abort()
+					return
+				}
+			}
+			c.Next()
+		}()
+	}
+}
