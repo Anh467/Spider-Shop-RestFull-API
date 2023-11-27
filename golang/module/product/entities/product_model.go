11b2ee6e0567d04f0f12bd45d6b2685aac4fe5e4
Nguyen Anh Viet <van123872000@gmail.com>
2023-11-27 14:50:40 +0700

    add

--- golang/module/product/entities/product_model.go
+++ golang/module/product/entities/product_model.go
@@ -1,17 +1,17 @@
 package entities
 
-import entitiesCate "SpiderShop-Restfull-API/module/category/entities"
+import entities_category "SpiderShop-Restfull-API/module/category/entities"
 
 type ProductModel struct {
-	ProductID int                         `json:"productid" gorm:"column:ProductID;primaryKey"`
-	CateID    int                         `json:"cateid" gorm:"column:CateID"`
-	Cate      entitiesCate.CateGetProduct `json:"cate" gorm:"foreignKey:CateID"`
-	Name      string                      `json:"name" gorm:"column:Name"`
-	Desc      string                      `json:"desc" gorm:"column:Desc"`
-	Image     string                      `json:"image" gorm:"column:Image"`
-	Status    string                      `json:"status" gorm:"column:Status;default:Normal"`
-	CreatedAt string                      `json:"createdat" gorm:"column:createdAt"`
-	UpdatedAt string                      `json:"updatedat" gorm:"column:updatedAt"`
+	ProductID int                              `json:"productid" gorm:"column:ProductID;primaryKey"`
+	CateID    int                              `json:"cateid" gorm:"column:CateID"`
+	Cate      entities_category.CateGetProduct `json:"cate" gorm:"foreignKey:CateID"`
+	Name      string                           `json:"name" gorm:"column:Name"`
+	Desc      string                           `json:"desc" gorm:"column:Desc"`
+	Image     string                           `json:"image" gorm:"column:Image"`
+	Status    string                           `json:"status" gorm:"column:Status;default:Normal"`
+	CreatedAt string                           `json:"createdat" gorm:"column:createdAt"`
+	UpdatedAt string                           `json:"updatedat" gorm:"column:updatedAt"`
 }
 
 func (ProductModel) TableName() string {
